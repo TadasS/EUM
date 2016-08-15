838a5513002d87071487c4f98eab1b310b48e783
Tadas <tadas.siaudvytis@gmail.com>
2016-08-16 00:22:10 +0300

    unittest directory cleaned

---
+++ src/AppBundle/Tests/Controller/apiControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class apiControllerTest extends WebTestCase
+{
+
+    public function testCompleteScenario()
+    {
+        // should write something here
+    }
+}
